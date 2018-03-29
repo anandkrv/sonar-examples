7ed16869fcf50372297a02437de1a713c536ae04
Kumar, Anand <Anand.Kumar2@in.unisys.com>
2018-03-29 04:56:39 -0400

    Sonar analysis for different type of projects and languages and modularity

---
+++ SonarSource-sonar-examples/JprgTest/src/com/krv/CollTest/B.java
@@ -0,0 +1,37 @@
+package com.krv.CollTest;
+
+import java.util.Arrays;
+
+public class B {
+
+  String s;
+  int sid;
+  /* (non-Javadoc)
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((s == null) ? 0 : s.hashCode());
+    result = prime * result + sid;
+    return result;
+  }
+  /*asdasjl*/
+  /* (non-Javadoc)
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (getClass() != obj.getClass()) return false;
+    B other = (B) obj;
+    if (s == null) {
+      if (other.s != null) return false;
+    } else if (!s.equals(other.s)) return false;
+    if (sid != other.sid) return false;
+    return true;
+  }
+  
+  
+
+}
